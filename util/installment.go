74102850da8ceba63bd4f85d9c92d24a6115b440
avinragh <avinragh@gmail.com>
2022-05-02 05:15:43 +0530

    Fixed intallment calculation function

--- util/installment.go
+++ util/installment.go
@@ -16,7 +16,7 @@ func GetInstallments(loanId int64, loanAmount float64, term int64) []*models.Ins
 		installment := &models.Installment{}
 		installment.ID = GetInt64Pointer(loanId)
 		installment.InstallmentAmount = &installmentAmount
-		installment.DueDate = GetDateTimePointer(strfmt.DateTime(today.AddDate(0, 0, int(term)*7)))
+		installment.DueDate = GetDateTimePointer(strfmt.DateTime(today.AddDate(0, 0, i*7)))
 		installment.State = constants.InstallmentStatusPending
 		installments = append(installments, installment)
 	}
